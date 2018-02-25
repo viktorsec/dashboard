9016f536edbe39cbd3599b79ebdb6b75898b4481
Viktor Sec <hey@viktorsec.com>
2018-02-25 21:04:24 +0100

    Add vacuumlabs eslint and dummy components

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+export const propTypes = {
+}
+
+export const defaultProps = {
+}
+
+export default class Header extends Component {
+  static propTypes = {
+    ...propTypes,
+  }
+
+  static defaultProps = {
+    ...defaultProps,
+  }
+
+  render() {
+    return (
+      <p>
+        <h1>Ružinovský Habitat</h1>
+      </p>
+    )
+  }
+}
