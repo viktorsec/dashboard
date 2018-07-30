0792831fc52c702ff249fe08b4154c89b71bd67a
Viktor Sec <hey@viktorsec.com>
2018-07-30 20:15:24 +0200

    Add styles for extreme temperatures

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -3,6 +3,8 @@ import cx from 'classnames'
 import {getCurrentWeather, getForecastWeather} from './../../services/xuApi'
 
 const TEN_MINUTES = 10 * 60 * 1000
+const EXTREME_HEAT_BOUNDARY = 28
+const EXTREME_COLD_BOUNDARY = 4
 
 export default class Weather extends Component {
   state = {}
@@ -22,13 +24,13 @@ export default class Weather extends Component {
     const currentPromise = getCurrentWeather(location)
     showCurrent && currentPromise.then((data) => {
       const {current} = data
-      this.setState({current});
+      this.setState({current})
     })
 
     const forecastPromise = getForecastWeather(location)
     showForecast && forecastPromise.then((data) => {
       const {forecast} = data
-      this.setState({forecast});
+      this.setState({forecast})
     })
   }
 
@@ -38,12 +40,17 @@ export default class Weather extends Component {
     console.log("state", this.state)
 
     if (showCurrent && current) {
-      const {temp_c} = current
+      const temperature = current.temp_c
       const text = current.condition.text
+
+      const isExtremeHeat = temperature >= EXTREME_HEAT_BOUNDARY
+      const isExtremeCold = temperature <= EXTREME_COLD_BOUNDARY
+      const temperatureLabel = {redLabel: isExtremeHeat, blueLabel: isExtremeCold}
+      const temperatureClassName = cx('extralarge', temperatureLabel)
  
       return (
         <div className="Module">
-          <div className="extralarge">{temp_c}°C</div>
+          <div className={temperatureClassName}>{temperature}°C</div>
           <div className="">
             <div>{location}</div>
             <div>{text}</div>
